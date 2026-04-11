47fa9702fee07137d2fd7039db95b18ffb255188
a-saber <ahs629862@gmail.com>
2026-04-11 19:00:54 +0200

    api 01

---
+++ bin/todo.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+
+void main()async{
+  Dio dio = Dio();
+  var response = await dio.post('https://ntitodo-production-b847.up.railway.app/api/login',
+    data: FormData.fromMap({
+      'username': 'ahmedsaber',
+      'password': '123456'
+    })
+  );
+
+  print(response.data.toString());
+
+}
