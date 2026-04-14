c18c24377e91e0cb0ad87d47d8369f808ad748d2
a-saber <ahs629862@gmail.com>
2026-04-14 18:17:40 +0200

    update main function to call newTask instead of login; enhance newTask error handling

--- bin/todo.dart
+++ bin/todo.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 
 void main()async{
-  login();
+  newTask();
 }
 
 login()async{
@@ -42,20 +42,30 @@ register()async{
 
 }
 newTask()async{
+  try{
     Dio dio = Dio();
-  var response = await dio.post(
-    'https://ntitodo-production-b847.up.railway.app/api/new_task',
-    data: FormData.fromMap({
-      'title': 'new from dart 01',
-      'description': 'new 01'
-    }),
-    options: Options(
-      headers: {
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc3NjE3OTM3MCwianRpIjoiZmUxOGFlYjktNjczZC00NDU5LTlmNzktMDQxYWM0ODhiNTYzIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6NTcsIm5iZiI6MTc3NjE3OTM3MCwiY3NyZiI6ImEyOGI3OGY2LTE2NGMtNGZjMS1iOTk3LTMwYjZlNGRjY2U3ZSIsImV4cCI6MTc3NjE4MDI3MH0.-JdLPACubgWqs6k7hIJlSeJt2g5IIon4UUD3DjP5jcA'
-      }
-    )
-  );
-
-  print(response.data.toString());
+    var result = await dio.post(
+      'https://ntitodo-production-b847.up.railway.app/api/new_task',
+      data: FormData.fromMap({
+        'title': 'task 1',
+        'description': 'task 1 desc'
+      }),
+      options: Options(
+        headers: {
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc3NjE4MzMwMCwianRpIjoiOTdiMDM5NmItNjUxZC00OGQxLTkyN2EtMWMzZmYxNjk5MzIyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6NTcsIm5iZiI6MTc3NjE4MzMwMCwiY3NyZiI6ImMzZTA2MDdhLWUzNjMtNDUwMC05MWQyLTY4YmJhZDk0ZjIwNCIsImV4cCI6MTc3NjE4NDIwMH0.SQxQSSgoMyNegPTGLY2q-Hwvy46KvTzq151deyzNI80'
+        }
+      )
+    );
+    print(result.data.toString());
+  }
+  catch(e){
+    
+    if(e is DioException){
+      print((e.response?.data as Map<String, dynamic>)['message']);
+    }
+    else{
+      print('error through login request: ${e.toString()}');
+    }
+  }
 
 }
