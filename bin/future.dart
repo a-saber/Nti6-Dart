b963a41758abb8ab4b7d92f73657f0ebf45f2f13
a-saber <ahs629862@gmail.com>
2026-04-14 14:58:30 +0200

    task

--- bin/future.dart
+++ bin/future.dart
@@ -1,24 +1,23 @@
-void main() async{
-  process1().then((v){
-    process4();
-  });
+void main() async {
+  process1();
   process2();
   process3();
 
 }
 
-Future process1() async{
-  print('start process 1');
-  await Future.delayed(Duration(seconds: 2));
-  print('end process 1');
+process1() {
+  print('start Getting Data 1');
+  // delay
+  print('End Getting Data 1');
 }
 process2(){
-  print('process 2');
+  print('Start Getting Data 2');
+  // delay
+  print('End Getting Data 2');
 }
 process3(){
   print('process 3');
 }
-process4(){
-  print('process 4');
-}
+
+
 
